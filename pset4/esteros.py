a4a4da57cf3774198d050c58735a6d81428eac6e
Waleed Lugod <lugodwaleed@gmail.com>
2023-11-30 01:04:30 +0800

    actually found the answer

--- pset4/esteros.py
+++ pset4/esteros.py
@@ -1,42 +1,11 @@
 #!/usr/bin/env python3
-
-class Vertex:
-    def __init__(self, label):
-        self.label = label
-        self.neighbors = []
-        self.dist = 0 # initialize estimation
-        self.predecessor = None # predecessor in longest path
-        
-    def addNeighbor(self, vertex, weight):
-        self.neighbors.append((vertex, weight))
-
 def widest_barge(n: int, waterways: list[tuple[int, int, int]]) -> list[int]:
     '''
     Given the number of locations `n` and a list of waterways, return a list
     of n-1 integers where the ith integer denotes the width of the largest
     possible barge that can safely travel from location 0 to location i.
     '''
-    # dijkstra
-
-    # Initialize graph. Running time: O(n)
-    graph = {}
-    for i in range(len(waterways)):
-        u = graph[waterways[i][0]] if waterways[i][0] in graph else Vertex(waterways[i][0])
-        v = graph[waterways[i][1]] if waterways[i][1] in graph else Vertex(waterways[i][1])
-
-        u.addNeighbor(v, waterways[i][2])
-        v.addNeighbor(u, waterways[i][2])
-
-        if waterways[i][0] not in graph:
-            graph[u.label] = u
-        if waterways[i][1] not in graph:
-            graph[v.label] = v
-
-    verticesMaxQueue = []
-
-    # print graph for testing
-    for key in graph:
-        print(f"key: {key}, value: {graph[key].label}, neighbors: {[[neighbor[0].label, neighbor[1]] for neighbor in graph[key].neighbors]}")
+    # prims
     return [0]
 
 
